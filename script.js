edada4a21a9205bd2f321d7422bc2d0b511cc467
winfreywekesa148-bit <winfreywekesa148@gmail.com>
2026-02-01 16:49:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const year = document.getElementById("year");
+const Month = document.getElementById("Month");
+const Date = document.getElementById("Date");
+const Gender = document.getElementById("Gender");
+const resultBox = document.getElementById("result-box");
+const akanName = document.getElementById("akan-name");
+
+const dateMonth{
+    jan
+}
+
+const choice

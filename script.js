331bc9ad6e94fa89098149a56bf13a7ef9f80f3e
winfreywekesa148-bit <winfreywekesa148@gmail.com>
2026-02-01 17:58:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const year = document.getElementById("year");
-const Month = document.getElementById("Month");
-const Date = document.getElementById("Date");
-const Gender = document.getElementById("Gender");
+const yearInput = document.getElementById("year");
+const MonthSelect = document.getElementById("Month");
+const DateSelect = document.getElementById("Date");
+const GenderSelect = document.getElementById("Gender");
 const resultBox = document.getElementById("result-box");
 const akanName = document.getElementById("akan-name");
 
-function c
+populateSelect(daySelect, 1, 31);
+populateSelect(monthSelect, 1, 12);
+populateSelect(yearSelect, 1900, 2025); 
+
+function calculateName(){
+    const year = yearInput.value;
+    const Month = MonthSelect.value;
+    const Date = DateSelect.value;
+    const Gender = GenderSelect.value;
+
+    
+
+}
 
